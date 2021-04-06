19a43c7986fd84b6be6b6723ec8aa77be42daf49
Nebojsa Balac <lacbaa95@gmail.com>
2021-04-06 21:11:42 +0200

    admin-front pocetak

---
+++ admin-frontend/src/app/model/log-in/log-in.model.ts
@@ -0,0 +1,15 @@
+import { UserRole } from "./log-in";
+
+export class LogInModel {
+  constructor(
+    private email: string,
+    private accessToken: string,
+    private id: number,
+    private role: string
+  ) {
+  }
+
+  getRole(): UserRole {
+    return this.role === "ROLE_SUPER_ADMIN" ? UserRole.ROLE_SUPER_ADMIN : UserRole.UNAUTHORIZED;
+  }
+}
---
+++ admin-frontend/src/app/model/log-in/log-in.ts
@@ -0,0 +1,15 @@
+export class LogIn {
+    private email: string;
+    private password: string;
+  
+    constructor(email: string, password: string) {
+      this.email = email;
+      this.password = password;
+    }
+  }
+  
+  
+  export enum UserRole{
+    UNAUTHORIZED,
+    ROLE_SUPER_ADMIN
+  }
